3272516e5c423690032c3810802bf9a210867ae4
Kat-Erina <nalchevanidzekaterina@gmail.com>
2026-05-07 13:47:24 +0400

    fixed map

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
  const [modalOpen, setModalOpen] = useState(false);
   const t=useTranslations('hero')
   return (
-    <section className="bg-[url(/hero.png)] bg-center h-[92vh] bg-cover flex flex-col justify-end gap-20 pl-25 pb-20 max-md:pl-0 max-md:items-center max-md:text-center relative">
+    <section className="bg-[url(/hero.webp)] bg-center h-[92vh] bg-cover flex flex-col justify-end gap-20 pl-25 pb-20 max-md:pl-0 max-md:items-center max-md:text-center relative">
       <div className="absolute inset-0 bg-linear-to-b from-[#66666670] to-[#00000080]" />
-      <h1 className="text-6xl md:text-6xl font-light  lg:text-[66px] z-10 leading-[110%] uppercase">
+      <h1 className="text-6xl md:text-6xl font-light  lg:text-[66px] !text-white z-10 leading-[110%] uppercase">
       {t('city')}<br /> {t('avenue')}
       </h1>
       <RequestCallBtn onClick={()=>{setModalOpen(true)}} text={t('requestCall')} background={"bg-[#E8E6DD]/15"} />
--- components/MapSection.jsx
+++ components/MapSection.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 
 const MapSection = () => {
   return (
-<Link href={'https://www.google.com/maps/@41.7044819,44.8703534,15z?authuser=0&entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D'}>
- <div className='bg-[url(/cta.png)] h-180 bg-cover bg-center bg-no-repeat'></div></Link>
+<Link href={'https://www.google.com/maps/@41.7044819,44.8703534,15z?authuser=0&entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D'}  className="block">
+
+  <div className="h-[500px] sm:h-180 overflow-hidden">
+    <img 
+      src="/cta.png" 
+      className="w-full h-full object-cover  sm:scale-100 origin-center"
+    />
+  </div>
+
+ </Link>
    
   )
 }
