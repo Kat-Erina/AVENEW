05e88bda67ac9d0ad452b7a3dd4ba900540558b8
Kat-Erina <nalchevanidzekaterina@gmail.com>
2026-05-07 16:27:22 +0400

    revised href

--- components/MapSection.jsx
+++ components/MapSection.jsx
@@ -5,7 +5,7 @@ const MapSection = () => {
 
   const mapUrl="https://www.google.com/maps/place/198+Davit+Aghmashenebeli+Ave,+T'bilisi/@41.7198337,44.7898696,20.75z/data=!4m13!1m7!3m6!1s0x404472d30367b303:0x1b6b470c93fc0fc3!2s198+Davit+Aghmashenebeli+Ave,+T'bilisi!3b1!8m2!3d41.7199082!4d44.7895896!3m4!1s0x404472d30367b303:0x1b6b470c93fc0fc3!8m2!3d41.7199082!4d44.7895896!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDUwMi4wIKXMDSoASAFQAw%3D%3D"
   return (
-<Link href={mapUrl}  className="block">
+<Link href={mapUrl} target="_blank" rel="noopener noreferrer"  className="block">
 
   <div className="h-[500px] sm:h-180 overflow-hidden">
     <img 
