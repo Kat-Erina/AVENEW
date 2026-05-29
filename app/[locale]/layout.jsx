14b5b8ddb96f3b5e725a2f556332c6b617a7abf7
Tornikesamkharadze <tornikesamkharadzee@gmail.com>
2026-05-29 12:47:55 +0400

    upw

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -19,7 +19,20 @@ export default async function LocaleLayout({ children, params }) {
 
   return (
     <html lang={locale}>
+      <head>
+        <Script id="gtm" strategy="beforeInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PDXLQN2L');
+        `}</Script>
+      </head>
       <body className={`antialiased overflow-x-hidden` }>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDXLQN2L"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
+        </noscript>
         <Script id="meta-pixel" strategy="afterInteractive">{`
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
