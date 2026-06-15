9ba327c2a3f0674795fc8ef2581687cfd9e1a131
Kat-Erina <nalchevanidzekaterina@gmail.com>
2026-06-15 16:23:22 +0400

    updated

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -17,7 +17,6 @@ import React from "react";
 export async function generateMetadata({ params }) {
   const { locale } = await params
   const t = await getTranslations({ locale, namespace: 'metadata' })
-
   return {
     title: t('title'),
     description: t('description'),
